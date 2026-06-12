80038cbcc93363ccb3da1e9e781b04207b114ca2
osfe <osfe@xxxlgroup.com>
2026-06-12 14:43:10 +0200

    Revert "Merge branch 'develop'"
    This reverts commit ece1b2164cd7167a7ae009a38e8dd62a24b8ce9d, reversing
    changes made to 11a5c31ced4c0a5f9ae560efdb4d5f1cb79bde9c.

--- src/screens/MapScreen/types.ts
+++
@@ -1,9 +0,0 @@
-import { GemName } from '../../components/GemShape';
-
-export interface GemMarker {
-  id: string;
-  name: GemName;
-  latitude: number;
-  longitude: number;
-  collected: boolean;
-}
